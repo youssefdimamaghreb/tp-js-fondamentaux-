54574232fb9acdeb5b76ce3a77ec1e5e025979bc
youssefdimamaghreb <youssefsg09@gmail.com>
2025-11-06 16:00:15 +0000

    feat: Maîtrise des Objets JS

--- script.js
+++ script.js
@@ -13,18 +13,57 @@ if ("motDePasseAttendu===motDePasseUtilisateur") {
 } else {
     console.log("Accès refusé. Mot de passe incorrect.");
 }
+
 `const nom = "Dupont"`
 `const prenom = "Alice"`
 `const age = 17`
 const moi = {
-    prenom: "VotrePrénom",
-    nom: "VotreNom",
+    prenom: "Martin",
+    nom: "Dupon",
     age: 18,
     competences: ["HTML", "CSS"]
 };
 console.log(moi);
 console.log(moi.prenom); // Affiche "VotrePrénom" 
-// "Je m'appelle [VotrePrénom] [VotreNom]."
-moi.age = 19;
- moi.ville = "Paris";
- `moi`
+//Mini-Défi 1 : Affichez la phrase "Je m'appelle [VotrePrénom] [VotreNom]." en utilisant les propriétés de l'objet.
+console.log("Je m'appelle " + moi.prenom + " " + moi.nom)
+console.log("Je m'appelle", moi.prenom, moi.nom)
+moi.ville = "Paris"
+console.log(moi)
+
+const notes =[12, 15, 9, 18]
+
+for (let i = 0; i < notes.length; i++) {
+    console.log("La note à l'indice " + i + " est " + notes[i]);
+}
+
+const classe = [
+    { prenom: 'Alice', age: 18 },
+    { prenom: 'Bob', age: 17 }
+];
+
+for (let i = 0; 1 < classe.length ; i++) {
+    if (classe[i].age >=18) {
+        console.log("Les élèves majeurs sont: " + classe.prenom)
+    } else {
+        console.log(classe[i].prenom + " est mineur")
+    }
+}
+
+function saluer(prenom) {
+    console.log("Bonjour", prenom)
+} 
+const saluer = (prenom) => {
+    console.log("Bonjour", prenom)
+}
+saluer("Martin")
+
+function estMajeur(age) {
+    return age >= 1;
+}
+
+const estMajeur = (age) => {
+    return age >= 18;
+}
+
+const estMajeur = (age) => age >= 18
